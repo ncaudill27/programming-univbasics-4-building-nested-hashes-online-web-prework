d0831a9f870b818e98e1b6e0b0d753ec47f42147
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-17 03:05:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      one: "one",
+	      names: "one",
 	      two: "two",
 	      three: "three",
 	      four: "four"
